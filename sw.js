f510e21fbaca032dc5473c90d95ddd0225261eb0
saeidmm68 <47969881+saeidmm68@users.noreply.github.com>
2026-07-07 15:13:37 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'fitpro-v1';
-const urlsToCache = ['/', '/index.html'];
+const urlsToCache = ['/MY-GYM/', '/MY-GYM/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
@@ -9,20 +9,18 @@ self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
 
-// Notification handler
 self.addEventListener('notificationclick', e => {
   e.notification.close();
-  e.waitUntil(clients.openWindow('/'));
+  e.waitUntil(clients.openWindow('/MY-GYM/'));
 });
 
-// Push notification
 self.addEventListener('push', e => {
   const data = e.data ? e.data.json() : {};
-  self.registration.showNotification(data.title || 'FitPro', {
+  self.registration.showNotification(data.title || 'MY GYM', {
     body: data.body || '',
-    icon: '/icon-192.png',
-    badge: '/icon-192.png',
-    tag: data.tag || 'fitpro',
+    icon: '/MY-GYM/icon-192.png',
+    badge: '/MY-GYM/icon-192.png',
+    tag: data.tag || 'mygym',
     vibrate: [200, 100, 200]
   });
 });
